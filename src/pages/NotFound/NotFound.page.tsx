3a6b0f1fb81ae00bab8f0a97f3ea3f98c239b3eb
nriin-rizeki <ipandani2505@gmail.com>
2023-03-09 10:03:51 +0700

    style(not-found): adjust to use daisyui

--- src/pages/NotFound/NotFound.page.tsx
+++ src/pages/NotFound/NotFound.page.tsx
@@ -1,9 +1,14 @@
+import { Link } from '@solidjs/router';
 import { Component } from 'solid-js';
 
 const NotFoundPage: Component = () => (
-  <section class="p-8 text-gray-700">
-    <h1 class="text-2xl font-bold">404: Not Found</h1>
-    <p class="mt-4">It's gone 😞</p>
+  <section class="flex h-screen flex-col items-center justify-center space-y-3 text-primary-content">
+    <h1 class="text-3xl font-bold italic">404: Not Found</h1>
+    <p class="!mb-5">It's gone 😞</p>
+
+    <Link class="link-secondary link" href="/">
+      Go back to home 🏡
+    </Link>
   </section>
 );
 
